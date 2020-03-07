20012f96dd73074661566efbff02d146b0c025e5
Jorge Gallegos <gallegosjorge403@gmail.com>
2020-03-07 01:00:17 -0800

    Finished blog app

--- users/views.py
+++ users/views.py
@@ -1,12 +1,18 @@
-from django.shortcuts import render
-from .forms import RegistrationForm
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
 
 def register(request):
     if request.method == 'POST':
-        form = RegistrationForm(request.POST)
+        form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
+            username = request.POST['username']
+            password = request.POST['password1']
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
+            return redirect('blog-index')
     else:
-        form = RegistrationForm(request.POST)
+        form = RegisterForm(request.POST)
 
     return render(request, 'users/register.html', {'form': form })
